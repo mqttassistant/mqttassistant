a1fdc365fcd51134e9faf13ab386d0fbafad7ade
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2022-02-13 11:14:56 +0100

    add name and group to components

--- mqttassistant/component/config.py
+++ mqttassistant/component/config.py
@@ -9,9 +9,21 @@ from pydantic import (
 from .base import BinarySensor, Sensor
 
 
+COMPONENT_GROUPS = ['sensor', 'binary_sensors']
+
+
 class ComponentConfig(BaseModel):
     sensor: Optional[Dict[str, Sensor]] = dict()
     binary_sensor: Optional[Dict[str, BinarySensor]] = dict()
 
     class Config:
         extra = Extra.forbid
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.set_names()
+
+    def set_names(self):
+        for group in COMPONENT_GROUPS:
+            for name, component in getattr(self, group, dict()).items():
+                component.set_name(name)
